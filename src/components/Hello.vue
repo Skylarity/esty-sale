3b6a70d777defb3b0fbfb4f3745536c0fa905c98
Skyler Rexroad <skyrex1095@gmail.com>
2017-06-04 15:27:05 -0600

    Started getting API stuff set up

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,53 +1,59 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<h1>Etsy Sale</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12">
+				<button class="btn btn-outline-success" @click="test">Test</button>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
 export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+	name: 'hello',
+	data () {
+		return {
+			msg: 'Welcome to Your Vue.js App'
+		}
+	},
+	methods: {
+		test() {
+			let xmlhttp = new XMLHttpRequest()
+
+			// https://openapi.etsy.com/v2/users/__SELF__/favorites
+			this.$http.get('https://openapi.etsy.com/v2/etsystore?api_key=r2hw6d0a6lynsom4rhtqa84t')
+				.then(response => {
+					console.log(response)
+				}, response => {
+					console.log('error', response)
+				});
+		}
+	}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 h1, h2 {
-  font-weight: normal;
+	font-weight: normal;
 }
 
 ul {
-  list-style-type: none;
-  padding: 0;
+	list-style-type: none;
+	padding: 0;
 }
 
 li {
-  display: inline-block;
-  margin: 0 10px;
+	display: inline-block;
+	margin: 0 10px;
 }
 
 a {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
